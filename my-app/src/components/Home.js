834db6e4d27284126d397ebc0a18e83a9d2b47de
Sanjana <pingali4@illinois.edu>
2025-08-05 23:19:52 -0500

    changes to website

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="segment"><Image /></div>
       <div className="segment">Hi, I'm Sanjana Pingali!</div>
       <div className="segment">
-        A Master's student in Electrical and Computer Engineering at the University of Illinois at Urbana-Champaign
+        I'm a Master's student in Electrical and Computer Engineering at the University of Illinois at Urbana-Champaign
       </div>
       <div className="segment">
       Previously, I completed my Bachelor's degree in Computer Engineering at the same university, graduating as a James Scholar and a member of the Eta Kappa Nu (HKN) Honor Society.
