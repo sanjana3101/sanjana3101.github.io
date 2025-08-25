a27650f5933ded45f7a038d6a1099dccfc7431bb
Sanjana <pingali4@illinois.edu>
2025-08-25 13:28:00 -0500

    update

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -4,19 +4,31 @@ import './Contact.css';
 const Contact = () => {
   return (
     <div className="contact-container">
-     <p> <b>Contact Me</b> </p>
-      <p>If you'd like to get in touch, feel free to reach out via any of the following:</p>
+      <h1 className="contact-title">Contact Me</h1>
+      <p className="contact-description">
+        If you'd like to get in touch, feel free to reach out via any of the following:
+      </p>
 
       <ul className="contact-list">
         <li>
-          Email: <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=sanjanapingali@gmail.com"
+          Email:{" "}
+          <a
+            href="mailto:sanjanapingali@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-          >sanjanapingali@gmail.com </a>
+          >
+            sanjanapingali@gmail.com
+          </a>
         </li>
         <li>
-          LinkedIn: <a href="https://www.linkedin.com/in/sanjana-pingali-1b06b5216/" target="_blank" rel="noopener noreferrer">My LinkedIn Profile</a>
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/sanjana-pingali-1b06b5216/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My LinkedIn Profile
+          </a>
         </li>
       </ul>
     </div>
